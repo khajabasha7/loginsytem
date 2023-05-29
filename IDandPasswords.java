22f13478359f4b11638177cc51442156f3e51196
khajabasha shaik <102409122+khajabasha7@users.noreply.github.com>
2023-05-29 21:58:45 +0530

    Update IDandPasswords.java

--- IDandPasswords.java
+++ IDandPasswords.java
@@ -4,12 +4,9 @@ public class IDandPasswords {
     HashMap<String,String> logininfo = new HashMap<String,String>();
     IDandPasswords()
     {
+        //logininfo.put("user","password");
         logininfo.put("khaja","munna");
-        logininfo.put("phani","wild");
-        logininfo.put("lavaya","lalu");
-        logininfo.put("navya","kolisetty");
-        logininfo.put("gowrish","123");
-        
+        //you add few username and password
     }
     protected HashMap<String, String> getLogininfo() {
         return logininfo;
